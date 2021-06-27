fdab3f0f46e02466ff372cf5cac86a3bcfc8e376
= <daine.bigham@gmail.com>
2021-06-27 16:26:32 +1200

    added functionality to query tequila flight database and search for the IATA codes for each city in the sheet, then update the iata codes column with the correct code for the city

--- flight_search.py
+++ flight_search.py
@@ -1,15 +1,19 @@
 import requests
+from pprint import pprint
+
+TEQUILA_URL = 'https://tequila-api.kiwi.com/locations/query'
+TEGUILA_HEADER = {
+    'apikey': ''
+}
 
 class FlightSearch:
     def __init__(self):
         pass
 
     def get_iata_code(self, city):
-        code = 'TESTING'
 
-        return code
+        response = requests.get(f'{TEQUILA_URL}?term={city}', headers=TEGUILA_HEADER)
+        data = response.json()
+        iata_code = data['locations'][0]['code']
 
-
-
-
-
+        return iata_code
